599c84ba7544e4c057640731f88db40844750456
yung1231 <pcymichael@gmail.com>
2023-04-11 19:59:00 +0800

    [Update] Modify the getDriver function

--- tools/tools.py
+++ tools/tools.py
@@ -3,6 +3,7 @@ import configparser
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.chrome.service import Service
+from selenium_stealth import stealth
 
 def readArgs():
 	parser = ArgumentParser()
@@ -22,12 +23,23 @@ def readConfig():
 
 def getDriver():
 	opts = ChromeOptions()
-	opts.add_argument("--start-maximized")
-	opts.add_argument("--incognito")
-	# opts.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+	opts.add_argument("start-maximized")
+	opts.add_argument("disable-infobars")
+	opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+	opts.add_experimental_option('useAutomationExtension', False)
 	ser = Service("./chromedriver.exe")
-
 	driver = Chrome(service=ser, options=opts)
+
+	stealth(driver,
+					user_agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+					languages= ["en-US", "en"],
+					vendor=  "Google Inc.",
+					platform=  "Win32",
+					webgl_vendor=  "Intel Inc.",
+					renderer=  "Intel Iris OpenGL Engine",
+					fix_hairline= False,
+					run_on_insecure_origins= False,
+	)
 	print('Window size: ', driver.get_window_size()) 
 		
 	return driver
