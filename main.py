f8cba2e901151bb59a6b8edaf03e4942b13ec501
yung1231 <pcymichael@gmail.com>
2023-04-13 11:20:59 +0800

    [Update] Modify usage

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ from tools.tools import readArgs, readConfig, getDriver
 if __name__=='__main__':
 	args = readArgs()
 	search = args.search
-	times = args.times if args.times<10 else 10
 	ttype = args.ttype
 	print('-'*20)
 	print('Type: ', ttype)
 	print('Search: ', search)
-	print('#Times: ', times)
 	print('-'*20)
 
 	config = readConfig()
-	save_pth = config['conf']['save_pth']
+	save_pth = config['conf']['save_pth']+f'\\{ttype}'
 	print('save_pth: ', save_pth)
 	
 	driver = getDriver()
 
-	pc = PCrawler(ttype, save_pth, search, times, driver)
+	pc = PCrawler(ttype, save_pth, search, driver)
 	pc.startSearch()
